aeac9c4a0170c55e6ba3177a820f61252bd80c92
PKPY <p01001011p01011001@gmail.com>
2024-04-02 05:47:47 +0330

    [dev][apps][admin]: Register models in admin panel

--- apps/signals/admin.py
+++ apps/signals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Signal, Comment, Reply, Report
 
-# Register your models here.
+
+admin.site.register(Signal)
+admin.site.register(Comment)
+admin.site.register(Reply)
+admin.site.register(Report)
