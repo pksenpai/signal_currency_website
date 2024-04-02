303517498c34edd1f148ac3a355f081d6cdd6247
PKPY <p01001011p01011001@gmail.com>
2024-04-02 10:09:20 +0330

    [dev][core][views]: Write Home page render

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 app_name = 'core'
 urlpatterns = [
-    # path('home/', ),
+    path('', Home.as_view(), name='home'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,18 @@
+from apps.signals.models import Signal
+
+from django.views import View
+
 from django.shortcuts import render
 
-# Create your views here.
+
+class Home(View):
+    template_name = 'core/home.html'
+    
+    def get(self, request):
+        signals = Signal.objects.all()
+        context = {
+            'signals': signals
+        }
+        
+        return render(request, self.template_name, context)
+
