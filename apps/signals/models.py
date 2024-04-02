e1b2ad6f782c8e6d4e716c1f20422bd8678116c1
PKPY <p01001011p01011001@gmail.com>
2024-04-02 03:53:00 +0330

    [dev][signals][models]: Write signals models by ERD

--- apps/signals/models.py
+++ apps/signals/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from apps.core.models import LogicalBaseModel, TimeStampBaseModel, StatusMixin
+from apps.user.models import Profile
 
-# Create your models here.
+
+class Signal(LogicalBaseModel, TimeStampBaseModel, StatusMixin):
+    
+    """\_____________[RELATIONS]_____________/"""
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='signals')
+    
+    """\_____________[MAIN]_____________/"""
+    title = models.CharField(max_length=30)
+    MARKETS_CHOICES = (
+        ('EIR', 'Iran Exchanges'),
+        ('CRP', 'Crypto'),
+    )
+    target_market = models.CharField(max_length=3, choices=MARKETS_CHOICES)
+    token = models.CharField(max_length=30)
+    
+    INVESTMENT_PERIOD_CHOICES = (
+        ('L', 'Long time'),
+        ('M', 'Mid time'),
+        ('S', 'Short time'),
+    )
+    investment_period = models.CharField(max_length=1, choices=INVESTMENT_PERIOD_CHOICES, null=True, blank=True)
+    
+    DIRECTION_CHOICES = (
+        ('U', 'Up'),
+        ('D', 'Down'),
+    )
+    direction = models.CharField(max_length=1, choices=DIRECTION_CHOICES, null=True, blank=True)
+    
+    fundamental_analysis = models.TextField()
+    technical_analysis = models.TextField()
+    price_action_analysis = models.TextField()
+    pa_time_frame = models.PositiveSmallIntegerField()
+    
+    hints = models.TextField()
+    like = models.IntegerField()
+    
